c6374e46c077e2f5d8770b7b5a9c1b3decfe3d39
Henry <hankbrown@gmail.com>
2016-06-18 22:15:24 -0700

    Fixed up publish_docs.sh

--- bin/publish_docs.sh
+++ bin/publish_docs.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
 if [ ! -d "gen" ]; then
-  echo "Generate docs first using:"  1>&2
-  echo "    ./bin/build_docs.sh"     1>&2
-fi
-
-if [ git diff-index --quiet HEAD -- ]; then
-  echo "Stash your changes first before trying to publish docs"
+  echo "Generate docs first using:"                             1>&2
+  echo "    ./bin/build_docs.sh"                                1>&2
 
   exit 1
 fi
 
+if [ git diff-index --quiet HEAD -- ]; then
+  echo "Stash your changes first before trying to publish docs" 1>&2
+
+  exit 1
+fi
+
+orphan="--orphan"
+
 SAVED_BRANCH=$(git branch | sed 's/\* //g')
 
-git stash
+if ! git checkout "$orphan" gh-pages; then
+  git checkout gh-pages
+fi
 
-git checkout --orphan gh-pages
+find . \
+  -not -path '.' \
+  -not -path '**/.git/**' \
+  -not -path '**/.git' \
+  -not -path '**/gen' \
+  -not -path '**/gen/**' \
+  -exec rm -rf {} \;
 
-git stash pop
+mv gen/* .
 
-git commit -m "gh-pages update $RANDOM"
+rm -rf gen
 
-git push
+git add .
 
-git checkout $SAVED_BRANCH
+git commit -m "gh-pages update ${RANDOM}.${RANDOM}"
+
+#git push
+
+#git checkout $SAVED_BRANCH
 
